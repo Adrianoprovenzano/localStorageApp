0fb2226de96b0391c0a68da3349399c8eab503eb
Adriano Provenzano <provenzanoadriano1@gmail.com>
2023-10-28 16:43:51 +0200

    added styles

--- src/index.js
+++ src/index.js
@@ -8,24 +8,53 @@ class UserCreator{
         this.number = number;
     }
 
-    sayHello() {
-        alert(`Thank you ${name} to have registered`);
+    userRegistered(id, name) {
+        const notificationDisplay = document.querySelector('#notificationDisplay');
+        notificationDisplay.innerHTML= `User with id: ${id} and name: ${name}, was successfully registered`;
+
+        setTimeout(() => { notificationDisplay.innerHTML= '';}, 2000)
     }
 }
 
 function createUser(id, name, address, number){
     const User = new UserCreator(id, name, address, number);
-    console.log(User);
     return User;
-
 }
 
-function addUserToPage(arr){
+function addUsersToPage(arr){
+    if('name' in arr){
+        let obj = arr;
+        let div = document.querySelector('#customerTable tbody');
+        div.innerHTML += `
+                <tr>
+                    <td>${obj.id}</td>
+                    <td> ${obj.name}</td>
+                    <td>${obj.address}</td>
+                    <td>${obj.number}</td>
+                    <td><button data-delete="d" id="${obj.id}" >Delete</button></td>
+                </tr>
+                `;
+    } else {
     for (let i in arr) {
-        let obj = JSON.parse(arr[i]); 
-        let div = document.querySelector('#logs');
-        div.innerHTML += `<p data-todoid="${obj.id}">${obj.id} | ${obj.name} | ${obj.address} | ${obj.number} <button data-delete="d" id="${obj.id}" >Delete</button> </p>`;
-    } 
+                let obj = JSON.parse(arr[i])
+                let div = document.querySelector('#customerTable tbody');
+                div.innerHTML += `
+                <tr>
+                    <td>${obj.id}</td>
+                    <td> ${obj.name}</td>
+                    <td>${obj.address}</td>
+                    <td>${obj.number}</td>
+                    <td><button data-delete="d" id="${obj.id}" >Delete</button></td>
+                </tr>
+                `;
+         }
+    }
+}
+
+function deleteUser(element){
+    let id = element.getAttribute('id');
+    localStorage.removeItem('User_'+ id);
+    element.parentElement.parentElement.remove();
 }
 
 function addToLocalStorage(user){
@@ -35,17 +64,23 @@ function addToLocalStorage(user){
     //Set LocalStorage string and parse it to object and add to LocalStorage
     const storageString = `{"id": "${array[0]}", "name": "${array[1]}", "address": "${array[2]}", "number": "${array[3]}"}`;
     const obj = JSON.parse(storageString);
-    localStorage.setItem('User'+ array[0], JSON.stringify(obj));
-    location.reload();
+    localStorage.setItem('User_'+ array[0], JSON.stringify(obj));
+    console.log('localstore finished')
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//             Event Handling
+
 //render LocalStorage on Page
-const allItemsInStorage = { ...localStorage };
-addUserToPage(allItemsInStorage);
+window.addEventListener('load', () => {
+    const allItemsInStorage = { ...localStorage };
+    addUsersToPage(allItemsInStorage);
+})
+
 
 //Form Submit 
 let inputs = [];
-
 const registerForm = document.querySelector('#registerForm');
 
 registerForm.addEventListener('submit', (e) => {
@@ -53,20 +88,15 @@ registerForm.addEventListener('submit', (e) => {
     const name = document.querySelector('#name').value;
     const address = document.querySelector('#address').value;
     const number = document.querySelector('#number').value;
+    inputs = [];
     inputs.push(id, name, address, number);
+    console.log(inputs)
     e.preventDefault();
     //creates a User Object
     const user = createUser(inputs[0], inputs[1], inputs[2], inputs[3]);
     addToLocalStorage(user);
+    addUsersToPage(user);
+    registerForm.reset();
+    user.userRegistered(user.id, user.name);
 })
 
-// Delete function, Localstorage
-const deleteItem = document.querySelector('button[data-delete="d"]');
-if(deleteItem){
-    deleteItem.addEventListener('click', (e) => {
-        e.preventDefault();
-        let b =deleteItem.getAttribute('id');
-        localStorage.removeItem('User'+ b);
-        location.reload();
-})
-}
